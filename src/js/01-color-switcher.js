ea2c954f6759780d1d00814c319da360b1ec185f
alxtpl <alxtpl@gmail.com>
2022-01-10 18:17:13 +0200

    adding first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// console.log(color);
+// .disabled = true;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyNode = document.querySelector('body');
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
 
+function onStart() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    console.log('start');
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+
+        console.log(`backgroundColor:  ${color}`);
+        bodyNode.style.backgroundColor = color;
+    }, 1000);
+}
+
+function onStop() {
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+    console.log('stop');
+    clearInterval(timerId);
+    console.log(`Interval with id ${timerId} has stopped!`);
+}
